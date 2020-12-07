9cc404c4a3559838867f75a0eed1ccab13d8f2f4
yycliang <YongYanLiang2018@gmail.com>
2020-12-07 16:20:04 -0500

    english fix

--- src/js/puzzles.js
+++ src/js/puzzles.js
@@ -1,40 +1,44 @@
 const puzzles = [
-    {
-        message: "That is the lock on your door. Make sure you've finished all your homework to escape! (Answer is a word.)",
-        puzzleImg: "https://images2.imgbox.com/68/ac/SmuttPDv_o.jpg",
-        answerImg: "https://i.postimg.cc/KcCsX59C/schoolanswer.png",
-        answer: "SCHOOL",
-    },
-    {
-        message: "Except from Homo Deus, written by Yuval Noah Harari. Fascinating book!(Answer is a word.)",
-        puzzleImg: "https://i.postimg.cc/MpRpqtb4/englishpuzzle.png",
-        answerImg: "https://i.postimg.cc/XJzdCQGW/englishanswer.png",
-        answer: "ELA",
-    },
-    {
-        message: "Who doesn't love typing? (Answer is a word.)",
-        puzzleImg: "https://i.postimg.cc/0jBy2B4g/compscipuzzle.jpg",
-        answerImg: "https://i.postimg.cc/633CqDQz/compscianswer.png",
-        answer: "COMPSCI",
-    },
-    {
-        message: "When did they start adding mirrors to the Chem curriculum?! (Answer is a word.)",
-        puzzleImg: "https://images2.imgbox.com/bd/81/5HSogXif_o.png",
-        answerImg: "https://i.postimg.cc/h4X33Y09/scienceanswer.png",
-        answer: "SCIENCE",
-    },
-    {
-        message: "What an interesting assignment you have there...(Answer is a word.)",
-        puzzleImg: "https://i.postimg.cc/KvC86CLB/mathpuzzle.png",
-        answerImg: "https://i.postimg.cc/rsRWCxxr/mathanswer.png",
-        answer: "MATH",
-    },
-    {
-        message: "Nice map. (Answer is a word.)",
-        puzzleImg: "https://i.postimg.cc/1zQ5t6vz/mappuzzle.jpg",
-        answerImg: "https://images2.imgbox.com/11/76/p7OLTqji_o.png",
-        answer: "HISTORY",
-    },
+  {
+    message:
+      "That is the lock on your door. Make sure you've finished all your homework to escape! (Answer is a word.)",
+    puzzleImg: "https://images2.imgbox.com/68/ac/SmuttPDv_o.jpg",
+    answerImg: "https://i.postimg.cc/KcCsX59C/schoolanswer.png",
+    answer: "SCHOOL",
+  },
+  {
+    message:
+      "Except from Homo Deus, written by Yuval Noah Harari. Fascinating book!(Answer is a word.)",
+    puzzleImg: "https://i.postimg.cc/ncr9B47X/englishpuzzle.png",
+    answerImg: "https://i.postimg.cc/XJzdCQGW/englishanswer.png",
+    answer: "ELA",
+  },
+  {
+    message: "Who doesn't love typing? (Answer is a word.)",
+    puzzleImg: "https://i.postimg.cc/0jBy2B4g/compscipuzzle.jpg",
+    answerImg: "https://i.postimg.cc/633CqDQz/compscianswer.png",
+    answer: "COMPSCI",
+  },
+  {
+    message:
+      "When did they start adding mirrors to the Chem curriculum?! (Answer is a word.)",
+    puzzleImg: "https://images2.imgbox.com/bd/81/5HSogXif_o.png",
+    answerImg: "https://i.postimg.cc/h4X33Y09/scienceanswer.png",
+    answer: "SCIENCE",
+  },
+  {
+    message:
+      "What an interesting assignment you have there...(Answer is a word.)",
+    puzzleImg: "https://i.postimg.cc/KvC86CLB/mathpuzzle.png",
+    answerImg: "https://i.postimg.cc/rsRWCxxr/mathanswer.png",
+    answer: "MATH",
+  },
+  {
+    message: "Nice map. (Answer is a word.)",
+    puzzleImg: "https://i.postimg.cc/1zQ5t6vz/mappuzzle.jpg",
+    answerImg: "https://images2.imgbox.com/11/76/p7OLTqji_o.png",
+    answer: "HISTORY",
+  },
 ];
 
 export { puzzles };
